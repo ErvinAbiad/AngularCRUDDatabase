f170717f2952ecdd8d6c534d71d9764a22ef7bb0
Ervin Abiad <ErvinA@magenic.com>
2023-01-18 04:32:50 +0800

    Implement Database

---
+++ AngularWebCrudDB/AngularWebCrudDB/dbo/Tables/Transaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Transaction] (
+    [TransactionID]   INT             IDENTITY (1, 1) NOT NULL,
+    [TransactionName] VARCHAR (500)   NULL,
+    [Cost]            DECIMAL (17, 2) NULL,
+    [Date]            DATE            NULL,
+    CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED ([TransactionID] ASC)
+);
+
